12221da7bc7a1952450d5e15772f01fb8c5fc3e4
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-08-11 21:24:55 -0300

    Introdução
    Introdução ao módulo começando por class.

---
+++ class.py
@@ -0,0 +1,11 @@
+
+
+
+class Pessoa:
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+pessoa = Pessoa('André', 'Ramos')
+print(pessoa.nome)
+print(pessoa.sobrenome)
