2fd78856cbc826ef1043c90f269f4b9d5c9b8f2e
AndreRamos-py <83097746+AndreRamos-py@users.noreply.github.com>
2023-08-15 16:07:59 -0300

    Instancias, parametros e argumentos
    Entendendo mais sobre o que é instancia, parâmetros e argumentos das classes.

--- class.py
+++ class.py
@@ -1,6 +1,6 @@
 
 
-
+'''
 class Pessoa:
     def __init__(self, nome, sobrenome):
         self.nome = nome
@@ -9,3 +9,16 @@ class Pessoa:
 pessoa = Pessoa('André', 'Ramos')
 print(pessoa.nome)
 print(pessoa.sobrenome)
+'''
+
+
+class Carro:
+    def __init__(self, carro):
+        self.nome = carro
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando!')
+
+mustang = Carro('Mustang')
+print(mustang.nome)
+mustang.acelerar()
